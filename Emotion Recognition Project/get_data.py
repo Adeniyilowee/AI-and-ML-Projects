4222e3197b50e9ff2dab6530919a405893eca6a7
adeniyim <mosakuadeniyi@gmail.com>
2023-01-09 13:04:29 +0100

    tr

---
+++ Emotion Recognition Project/get_data.py
@@ -0,0 +1,4 @@
+# You can use the link below to get the data
+# The folders are named Train and Test. They should be pasted in the same folder with the jupyter notebook
+
+# https://drive.google.com/file/d/1lHYIoBLQT0AtZePUIzMItASgWY7tSwIs/view?usp=share_link
